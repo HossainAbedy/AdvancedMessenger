6b5c17239b1da6ded78ac3c9f9a1b8c675e4c2c5
Hossain Abedy <abedy.ewu@gmail.com>
2019-07-05 02:05:41 +0600

    n

--- public/js/app.js
+++ public/js/app.js
@@ -63594,8 +63594,8 @@ if (token) {
 window.Pusher = __webpack_require__(/*! pusher-js */ "./node_modules/pusher-js/dist/web/pusher.js");
 window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__["default"]({
   broadcaster: 'pusher',
-  key: "d2503347d755b9309403",
-  cluster: "ap2",
+  key: "",
+  cluster: "mt1",
   encrypted: true
 });
 
@@ -64141,8 +64141,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\Advanced-MyMessenger\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\Advanced-MyMessenger\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\mymessenger\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\mymessenger\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
